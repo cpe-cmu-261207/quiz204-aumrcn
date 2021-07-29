ca71fc34d4ed8351377fe27360cd6e914b2edb79
aumrcn <aumkung20728@gmail.com>
2021-07-29 14:16:17 +0700

    commit 1

--- script.js
+++ script.js
@@ -5,9 +5,26 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
+const text = document.querySelector('p').textContent;
+var btn_boo = true;
+const ogAuthor = author.textContent;
+// var str = 
 
 btn_toggle.onclick = () => {
   // your code here
+  const bobo = 630610756 - length.value;
+  console.log(text[0])
+  if(btn_boo == true){
+    author.textContent = bobo;
+    btn_boo = false;
+    btn_toggle.textContent = "Display Author";
+  }else{
+    author.textContent = ogAuthor;
+    btn_boo = true;
+    btn_toggle.textContent = "Display Calculation";
+  }
 }
+
+
 
 // more codes for Search and Reset buttons here
